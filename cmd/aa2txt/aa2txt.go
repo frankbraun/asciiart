c5f50cfb4550529fa27cbcf61c42f50b8e109c1e
Frank Braun <frank@cryptogroup.net>
2016-01-27 15:33:36 +0000

    simplify aa2txt

--- cmd/aa2txt/aa2txt.go
+++ cmd/aa2txt/aa2txt.go
@@ -6,31 +6,59 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 
-	"github.com/frankbraun/asciiart/convert"
+	"github.com/frankbraun/asciiart/aa2d"
 )
 
-func fatal(err error) {
-	fmt.Fprintf(os.Stderr, "%s: error: %s\n", os.Args[0], err)
-	os.Exit(1)
-}
-
-func usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s [arguments...]\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "Read ASCII art diagram from stdin and write tree to stdout.\n")
-	flag.PrintDefaults()
-	os.Exit(1)
-}
-
 func main() {
-	flag.Parse()
-	if flag.NArg() != 0 {
-		usage()
+	asciiArt, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
 	}
-	if err := convert.ASCIIArt2Txt(os.Stdout, os.Stdin); err != nil {
-		fatal(err)
+	toTxt(string(asciiArt))
+}
+
+func toTxt(asciiArt string) {
+	p := aa2d.NewParser()
+	p.SetScale(1, 1)
+	grid, err := p.Parse(asciiArt)
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmtFlt := func(f float64) string { return strconv.FormatFloat(f, 'f', -1, 64) }
+	var traverse func(elems []interface{}, indent string)
+	fmt.Println("grid", grid.W, grid.H)
+	traverse = func(elems []interface{}, indent string) {
+		for _, elem := range elems {
+			switch t := elem.(type) {
+			case *aa2d.Rectangle:
+				fmt.Println(indent, "rect", t.X, t.Y, t.W, t.H)
+				traverse(t.Elems, indent+"  ") // recursion
+			case *aa2d.Line:
+				fmt.Println(indent, "line", t.X1, t.Y1, t.X2, t.Y2)
+			case *aa2d.Polyline:
+				var p []string
+				for i := 0; i < len(t.X); i++ {
+					p = append(p, fmtFlt(t.X[i]), fmtFlt(t.Y[i]))
+				}
+				fmt.Println(indent, "polyline", strings.Join(p, " "))
+			case *aa2d.Polygon:
+				var p []string
+				for i := 0; i < len(t.X); i++ {
+					p = append(p, fmtFlt(t.X[i]), fmtFlt(t.Y[i]))
+				}
+				fmt.Println(indent, "polygon", strings.Join(p, " "))
+				traverse(t.Elems, indent+"  ") // recursion
+			case *aa2d.Textline:
+				fmt.Println(indent, "textline", t.X, t.Y, t.Text)
+			}
+		}
+	}
+	traverse(grid.Elems, " ")
 }
--- convert/convert.go
+++ convert/convert.go
@@ -7,11 +7,8 @@
 package convert
 
 import (
-	"fmt"
 	"io"
 	"io/ioutil"
-	"strconv"
-	"strings"
 
 	"github.com/frankbraun/asciiart/aa2d"
 	"github.com/frankbraun/asciiart/svg"
@@ -37,59 +34,5 @@ func ASCIIArt2SVG(w io.Writer, r io.Reader, xScale, yScale float64) error {
 	if err := svg.Generate(w, grid); err != nil {
 		return err
 	}
-	return nil
-}
-
-// ASCIIArt2Txt converts two-dimensional ASCII art read from r to text
-// written to w.
-func ASCIIArt2Txt(w io.Writer, r io.Reader) error {
-	aa, err := ioutil.ReadAll(r)
-	if err != nil {
-		return err
-	}
-	err = generateTxt(w, string(aa))
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func generateTxt(w io.Writer, aa string) error {
-	p := aa2d.NewParser()
-	p.SetScale(1, 1)
-	grid, err := p.Parse(aa)
-	if err != nil {
-		return err
-	}
-	fmtFlt := func(f float64) string { return strconv.FormatFloat(f, 'f', -1, 64) }
-	var traverse func(w io.Writer, elems []interface{}, indent string)
-	traverse = func(w io.Writer, elems []interface{}, indent string) {
-		for _, elem := range elems {
-			switch t := elem.(type) {
-			case *aa2d.Rectangle:
-				fmt.Fprintln(w, indent, "rect", t.X, t.Y, t.W, t.H)
-				traverse(w, t.Elems, indent+"  ") // recursion
-			case *aa2d.Line:
-				fmt.Fprintln(w, indent, "line", t.X1, t.Y1, t.X2, t.Y2)
-			case *aa2d.Polyline:
-				var p []string
-				for i := 0; i < len(t.X); i++ {
-					p = append(p, fmtFlt(t.X[i]), fmtFlt(t.Y[i]))
-				}
-				fmt.Fprintln(w, indent, "polyline", strings.Join(p, " "))
-			case *aa2d.Polygon:
-				var p []string
-				for i := 0; i < len(t.X); i++ {
-					p = append(p, fmtFlt(t.X[i]), fmtFlt(t.Y[i]))
-				}
-				fmt.Fprintln(w, indent, "polygon", strings.Join(p, " "))
-				traverse(w, t.Elems, indent+"  ") // recursion
-			case *aa2d.Textline:
-				fmt.Fprintln(w, indent, "textline", t.X, t.Y, t.Text)
-			}
-		}
-	}
-	fmt.Fprintln(w, "grid", grid.W, grid.H)
-	traverse(w, grid.Elems, " ")
 	return nil
 }
