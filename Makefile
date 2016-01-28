8439301879cf866fc0a2b04e1bf195c22d654c78
Frank Braun <frank@cryptogroup.net>
2016-01-28 13:18:56 +0000

    generate aa2txt.go

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 all:
-	go install -v github.com/frankbraun/asciiart/cmd/...
+	go install -v github.com/frankbraun/asciiart/...
 
-.PHONY: test
-test:
+.PHONY: generate test
+generate: all templates/*.tmpl
+	aatmpl
+
+test: all
 	go test -v github.com/frankbraun/asciiart/aa2d
---
+++ util/cmd/aatmpl/aatmpl.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2016 Frank Braun <frank@cryptogroup.net>
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// aatmpl is the code and documentation generator for the ASCII art package.
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"text/template"
+)
+
+func fatal(err error) {
+	fmt.Fprintf(os.Stderr, "%s: error: %s\n", os.Args[0], err)
+	os.Exit(1)
+}
+
+func main() {
+	// work around defer not working after os.Exit()
+	if err := aatmplMain(); err != nil {
+		fatal(err)
+	}
+}
+
+func aatmplMain() error {
+	templates := filepath.Join("templates", "*.tmpl")
+	tmpl, err := template.ParseGlob(templates)
+	if err != nil {
+		return err
+	}
+
+	/*
+		err = tmpl.ExecuteTemplate(os.Stdout, "aa2txt.tmpl", map[string]string{
+			"MainFunc": "main()",
+		})
+	*/
+
+	// generate cmd/aa2txt/aa2txt.go
+	fp, err := os.Create(filepath.Join("cmd", "aa2txt", "aa2txt.go"))
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+	err = tmpl.ExecuteTemplate(fp, "aa2txt.tmpl", map[string]string{
+		"MainFunc": "toTxt(asciiArt string)",
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
